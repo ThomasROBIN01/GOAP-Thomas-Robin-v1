3ab69edadc6b97c75841e8b36ccdae8ca718f427
Thomas <thomas-robin@hotmail.fr>
2026-02-12 11:13:44 +0800

    Filling up G_Condition

--- Assets/Scripts/GOAP/Conditions/G_Condition.cs
+++ Assets/Scripts/GOAP/Conditions/G_Condition.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
 
-public class G_Condition
+namespace GOAP
 {
-    
+    public class G_Condition
+    {
+        #region Variables
+
+        G_State state;  // the state being tested by the condition
+        public G_State State
+        {
+            get { return state; }   // we will be able to read the value through this property but not able to set it
+        }
+
+        G_StateComparison comparison;   // the actual comparison for the condition
+        public G_StateComparison Comparison
+        {
+            get { return comparison; }
+        }
+
+        object expectedValue;   // the value we will be comparing to the current value in the state
+        public object ExpectedValue
+        {
+            get { return expectedValue; }
+        }
+
+        bool met = false;   // has the condition been met during planning?
+        public bool Met
+        {
+            get { return met; }
+        }
+
+        #endregion
+    }
 }

9072fb0f8c6b2c2a26b4fab03768c90cce4d53d5
Thomas <thomas-robin@hotmail.fr>
2026-04-09 10:42:49 +0800

    Session 3 - 1l. Fixing Failed Tests and Inventory Condition Tests
    - Refactor InventoryStateTests: rename and change parametrized test (InventoryCanTestState -> InventoryTestState) to include itemInInventory, adjust inventory setup and expected value handling, and fix how items are added to the inventory (use expectedQuantity).
    - Add a new parameterized test InventoryConditionCompare to cover multiple condition/compare scenarios and implement comparison logic.

--- Assets/Tests/Edit Mode Tests/InventoryStateTests.cs
+++ Assets/Tests/Edit Mode Tests/InventoryStateTests.cs
@@ -13,12 +13,12 @@ public class InventoryStateTests
         
     }
 
-    [TestCase(true, true, true, 1, true, TestName = "All valid")]
-    [TestCase(false, true, true, 1, false, TestName = "State invalid")]
-    [TestCase(true, false, true, 1, false, TestName = "Expected Value invalid")]
-    [TestCase(true, true, false, 1, false, TestName = "Item invalid")]
-    [TestCase(true, true, false, 0, true, TestName = "Item invalid and Expected Value 0")]
-    public void InventoryCanTestState(bool stateIsValid, bool expectedIsValid, bool itemIsValid, int expectedQuantity, bool expectedResult)
+    [TestCase(true, true, true, true, 1, true, TestName = "All valid")]
+    [TestCase(false, true, true, true, 1, false, TestName = "State invalid")]
+    [TestCase(true, false, true, true, 1, false, TestName = "Expected Value invalid")]
+    [TestCase(true, true, false, true, 1, false, TestName = "Item invalid")]
+    [TestCase(true, true, true, false, 0, true, TestName = "Item invalid and Expected Value 0")]
+    public void InventoryTestState(bool stateIsValid, bool expectedIsValid, bool itemIsValid, bool itemInInventory, int expectedQuantity, bool expectedResult)
     {
         GameObject go = new GameObject();       // the reason to do make a GameObject is because we need to add an inventory component
         Inventory inventory = go.AddComponent<Inventory>();
@@ -33,14 +33,17 @@ public class InventoryStateTests
         {
             testState = An.InventoryState("test").WithInventory(inventory);
         }
-        if (expectedIsValid)
-        {
-            expectedValue = new ItemStack(testItem, expectedQuantity);
-        }
         if (itemIsValid)
         {
             testItem = An.Item("axe").IsStackable(false);
-            inventory.AddToInventory(new ItemStack(testItem, 1));
+        }
+        if (itemInInventory)
+        {
+            inventory.AddToInventory(new ItemStack(testItem, expectedQuantity));
+        }
+        if (expectedIsValid)
+        {
+            expectedValue = new ItemStack(testItem, expectedQuantity);
         }
 
         bool result = testState.TestState(testState, G_StateComparison.equal, expectedValue);
@@ -48,15 +51,27 @@ public class InventoryStateTests
         Assert.AreEqual(expectedResult, result);
     }
 
-    [TestCase (TestName = "Perfect Pass")]
-    public void InventoryTestState()
+    [TestCase (true, G_StateComparison.equal, 1, G_StateComparison.equal, 1, true, TestName = "Both equal to 1 item")]
+    [TestCase(true, G_StateComparison.equal, 1, G_StateComparison.equal, 0, false, TestName = "Not equal to same quantity")]
+    [TestCase(true, G_StateComparison.equal, 1, G_StateComparison.equal, 1, true, TestName = "Different Items")]
+    [TestCase(true, G_StateComparison.equal, 1, G_StateComparison.greater, 0, true, TestName = "Equal 1 vs Greater 0")]
+    public void InventoryConditionCompare(bool useSameItem, G_StateComparison preCompare, int preQuantity, G_StateComparison effectCompare, int effectQuantity, bool expectedResult)
     {
-        // Use the Assert class to test conditions
-    }
+        GameObject go = new GameObject();       // the reason to do make a GameObject is because we need to add an inventory component
+        Inventory inventory = go.AddComponent<Inventory>();
+        Item axe = An.Item("axe").IsStackable(false);
+        Item wood = An.Item("axe").IsStackable(true);
+        G_Inventory testState = An.InventoryState("test").WithInventory(inventory);
 
-    [Test]
-    public void InventoryConditionCompare()
-    {
-        // Use the Assert class to test conditions
+        ItemStack preExpectedValue = new ItemStack(axe, 1);
+        ItemStack effectExpectedValue = useSameItem? new ItemStack(axe, 1): new ItemStack(wood, 1);
+
+
+        G_Condition precondition = A.Condition().WithState(testState).WithComparison(preCompare).WithExpectedValue (effectExpectedValue);
+        G_Condition effect = A.Condition().WithState(testState).WithComparison(effectCompare).WithExpectedValue(effectExpectedValue);
+
+        bool result = precondition.CompareConditionToEffect(effect);
+
+        Assert.AreEqual(expectedResult, result);
     }
 }

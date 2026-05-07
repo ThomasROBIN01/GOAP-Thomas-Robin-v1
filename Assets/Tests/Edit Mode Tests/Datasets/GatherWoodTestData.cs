1b14869f72c4448342c4d3587f50de684755d306
Thomas <thomas-robin@hotmail.fr>
2026-05-07 11:19:29 +0800

    Session 4 - 3b. Testing Dataset - Constructor
    - Reorganize and consolidate test data for GatherWood tests: moved world-state declaration, introduced a GameObjects region, and added a constructor that initializes items, location types, GameObjects, Inventory components, and GOAP state objects.
    This centralizes setup logic for the tests and eliminates duplicated/fragmented field declarations so instances are ready for use in edit-mode tests.

--- Assets/Tests/Edit Mode Tests/Datasets/GatherWoodTestData.cs
+++ Assets/Tests/Edit Mode Tests/Datasets/GatherWoodTestData.cs
@@ -8,9 +8,7 @@ namespace GOAP
     {
         public const int woodStockMax = 9999;   // const: to keep it as it is and not be able to modify it
 
-        #region World States
-        public G_WorldState npc_world_state;
-        #endregion
+        #region Data and Objects
 
         #region Items
         public Item chopped_wood;
@@ -25,6 +23,14 @@ namespace GOAP
         public LocationType shop;
         #endregion
 
+        #region GameObjects
+        public GameObject npc_object;
+        public GameObject tree_object;
+        public GameObject workshop_object;
+        public GameObject woodstock_object;
+        public GameObject shop_object;
+        #endregion
+
         #region Inventories
         public Inventory npc_inventory_component;
         public Inventory workshop_inventory_component;
@@ -33,13 +39,9 @@ namespace GOAP
         public Inventory shop_inventory_component;
         #endregion
 
-        #region GameObjects
-        public GameObject npc_object;
-        public GameObject tree_object;
-        public GameObject workshop_object;
-        public GameObject woodstock_object;
-        public GameObject shop_object;
         #endregion
+
+        #region GOAP
 
         #region States
         public G_Inventory npc_inventory;
@@ -65,6 +67,56 @@ namespace GOAP
         public G_Goal gather_wood;
         #endregion
 
+        #region World States
+        public G_WorldState npc_world_state;
+        #endregion
+
+        #endregion
+
+        public GatherWoodTestData()
+        {
+            #region Data Creation
+
+            chopped_wood = An.Item("chopped_wood").IsStackable(true);
+            axe = An.Item("chopped_wood").IsStackable(false);
+            money = An.Item("chopped_wood").IsStackable(true);
+
+            tree = A.LocationType("tree");
+            workshop = A.LocationType("workshop");
+            woodstock = A.LocationType("woodstock");
+            shop = A.LocationType("shop");
+
+            #endregion
+
+            #region Objects and Component Creation
+
+            npc_object = new GameObject();
+            tree_object = new GameObject();
+            workshop_object = new GameObject();
+            woodstock_object = new GameObject();
+            shop_object = new GameObject();
+
+            npc_inventory_component = npc_object.AddComponent<Inventory>();
+            tree_inventory_component = tree_object.AddComponent<Inventory>();
+            workshop_inventory_component = workshop_object.AddComponent<Inventory>();
+            woodstock_inventory_component = woodstock_object.AddComponent<Inventory>();
+            shop_inventory_component = shop_object.AddComponent<Inventory>();
+
+            #endregion
+
+
+            #region State Creation
+
+            npc_inventory = An.InventoryState("npc_inventory").WithInventory(npc_inventory_component);
+            workshop_inventory = An.InventoryState("workshop_inventory").WithInventory(workshop_inventory_component);
+            woodstock_inventory = An.InventoryState("woodstock_inventory").WithInventory(woodstock_inventory_component);
+            tree_inventory = An.InventoryState("tree_inventory").WithInventory(tree_inventory_component);
+            shop_inventory = An.InventoryState("shop_inventory").WithInventory(shop_inventory_component);
+            atLocation = An.AtLocation().WithName("at_location");
+
+            #endregion
+
+        }
 
     }
 }

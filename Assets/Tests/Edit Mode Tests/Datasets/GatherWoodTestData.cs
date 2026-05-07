448b011a83aa59fb8aa484ca4b4c89de665f43b2
Thomas <thomas-robin@hotmail.fr>
2026-05-07 12:17:45 +0800

    Session 4 - 3c. Testing Dataset - Creating the Actions
    - Add an Action Generation region to the GatherWood test dataset, defining actions used by the planner (deliver_wood, go_to_woodstock, chop_tree, go_to_tree, take_axe, go_to_workshop, buy_wood, go_to_shop) with their preconditions, effects, and costs to expand the test scenario for gathering and delivering wood.

--- Assets/Tests/Edit Mode Tests/Datasets/GatherWoodTestData.cs
+++ Assets/Tests/Edit Mode Tests/Datasets/GatherWoodTestData.cs
@@ -49,7 +49,7 @@ namespace GOAP
         public G_Inventory woodstock_inventory;
         public G_Inventory tree_inventory;
         public G_Inventory shop_inventory;
-        public G_AtLocation atLocation;
+        public G_AtLocation at_location;
         #endregion
 
         #region Actions
@@ -112,10 +112,122 @@ namespace GOAP
             woodstock_inventory = An.InventoryState("woodstock_inventory").WithInventory(woodstock_inventory_component);
             tree_inventory = An.InventoryState("tree_inventory").WithInventory(tree_inventory_component);
             shop_inventory = An.InventoryState("shop_inventory").WithInventory(shop_inventory_component);
-            atLocation = An.AtLocation().WithName("at_location");
+            at_location = An.AtLocation().WithName("at_location");
 
             #endregion
 
+            #region Action Generation
+
+            deliver_wood = An.Action("deliver_wood").WithPrecondition(A.Condition().WithState(npc_inventory)
+                                                                                   .WithComparison(G_StateComparison.greaterOrEqual)
+                                                                                   .WithExpectedValue(new ItemStack(chopped_wood, 10)))     // to deliver wood, we need to have more than 10 chopped wood
+
+                                                    .WithPrecondition(A.Condition().WithState(at_location)
+                                                                                   .WithExpectedValue(woodstock))       // we need to be at the woodstock location
+
+
+
+                                                    .WithEffect(A.Condition().WithState(woodstock_inventory)
+                                                                             .WithComparison(G_StateComparison.equal)
+                                                                             .WithExpectedValue(new ItemStack(chopped_wood, woodStockMax)))     // we'll just have the woodstock inventory filled up as an effect for this test
+
+                                                    .WithEffect(A.Condition().WithState(npc_inventory)
+                                                                             .WithComparison(G_StateComparison.equal)
+                                                                             .WithExpectedValue(ItemStack.EmptyStack(chopped_wood)))    // the npc inventory will be emptied of the chopped wood
+
+                                                    .WithCost(10);
+
+
+            go_to_woodstock = An.Action("go_to_woodstock").WithEffect(A.Condition().WithState(at_location).WithExpectedValue(woodstock))
+                                                          .WithCost(10);
+
+
+            chop_tree = An.Action("chop_tree").WithPrecondition(A.Condition().WithState(npc_inventory)
+                                                                             .WithComparison(G_StateComparison.greater)
+                                                                             .WithExpectedValue(ItemStack.EmptyStack(axe)))       // put an axe in the npc inventory
+
+                                              .WithPrecondition(A.Condition().WithState(at_location)
+                                                                             .WithExpectedValue(tree))        // we need to be at the tree location to cut it
+
+
+
+                                              .WithEffect(A.Condition().WithState(npc_inventory)
+                                                                       .WithComparison(G_StateComparison.greaterOrEqual)
+                                                                       .WithExpectedValue(new ItemStack(chopped_wood, 10)))   // chopping a tree will give 10 chopped wood
+
+                                              .WithEffect(A.Condition().WithState(tree_inventory)
+                                                                       .WithComparison(G_StateComparison.equal)
+                                                                       .WithExpectedValue(ItemStack.EmptyStack(chopped_wood))) // the tree inventory will be fully emptied for this test
+
+                                              .WithCost(10);
+
+
+            go_to_tree = An.Action("go_to_tree").WithEffect(A.Condition().WithState(at_location).WithExpectedValue(tree))
+                                                .WithCost(10);
+
+
+            take_axe = An.Action("take_axe").WithPrecondition(A.Condition().WithState(npc_inventory)
+                                                                           .WithComparison(G_StateComparison.equal)
+                                                                           .WithExpectedValue(ItemStack.EmptyStack(axe)))   // we need the npc inventory to have no axe to be able to take one
+
+                                            .WithPrecondition(A.Condition().WithState(workshop_inventory)
+                                                                           .WithComparison(G_StateComparison.greater)
+                                                                           .WithExpectedValue(ItemStack.EmptyStack(axe)))   // workshop inventory needs to have one axe available
+
+                                            .WithPrecondition(A.Condition().WithState(at_location).WithExpectedValue(workshop))     // we need to be at the workshop to take the axe
+
+
+
+                                            .WithEffect(A.Condition().WithState(npc_inventory)
+                                                                     .WithComparison(G_StateComparison.greater)
+                                                                     .WithExpectedValue(ItemStack.EmptyStack(axe)))   // axe will be greater than 0 in the npc_inventory
+
+                                            .WithEffect(A.Condition().WithState(workshop_inventory)
+                                                                     .WithComparison(G_StateComparison.equal)
+                                                                     .WithExpectedValue(ItemStack.EmptyStack(axe)))   // the workshop will now have no axe in for this test
+
+                                            .WithCost(10);
+
+
+            go_to_workshop = An.Action("go_to_workshop").WithEffect(A.Condition().WithState(at_location).WithExpectedValue(workshop))
+                                    .WithCost(10);
+
+
+            buy_wood = An.Action("buy_wood").WithPrecondition(A.Condition().WithState(npc_inventory)
+                                                                           .WithComparison(G_StateComparison.equal)
+                                                                           .WithExpectedValue(ItemStack.EmptyStack(chopped_wood)))   // we need the npc inventory to have no chopped wood to be able to buy some
+
+                                            .WithPrecondition(A.Condition().WithState(npc_inventory)
+                                                                           .WithComparison(G_StateComparison.greater)
+                                                                           .WithExpectedValue(ItemStack.EmptyStack(money)))   // we also need the npc inventory to have some money (>0)
+
+                                            .WithPrecondition(A.Condition().WithState(shop_inventory)
+                                                                           .WithComparison(G_StateComparison.greater)
+                                                                           .WithExpectedValue(ItemStack.EmptyStack(chopped_wood)))   // workshop inventory needs to have some chopped wood (>0)
+
+                                            .WithPrecondition(A.Condition().WithState(at_location).WithExpectedValue(shop))     // we need to be at the shop
+
+
+
+                                            .WithEffect(A.Condition().WithState(npc_inventory)
+                                                                     .WithComparison(G_StateComparison.greaterOrEqual)
+                                                                     .WithExpectedValue(new ItemStack(chopped_wood, 10)))   // the npc inventory will then have at least 10 chopped wood
+
+                                            .WithEffect(A.Condition().WithState(shop_inventory)
+                                                                     .WithComparison(G_StateComparison.equal)
+                                                                     .WithExpectedValue(ItemStack.EmptyStack(chopped_wood)))   // the shop will be emptied of chopped wood for this test
+
+                                            .WithEffect(A.Condition().WithState(npc_inventory)
+                                                                     .WithComparison(G_StateComparison.equal)
+                                                                     .WithExpectedValue(ItemStack.EmptyStack(money)))   // the npc inventory will have no money after buying the wood for this test
+
+                                            .WithCost(10);
+
+
+            go_to_shop = An.Action("go_to_shop").WithEffect(A.Condition().WithState(at_location).WithExpectedValue(shop))
+                        .WithCost(10);
+
+            #endregion
         }
 
     }
